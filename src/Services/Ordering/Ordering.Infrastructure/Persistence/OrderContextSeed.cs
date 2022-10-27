3aef0abe4a0d16312f03f322c4a19e21c05965b7
Gourav <dwivedi.gourav@gmail.com>
2022-10-27 03:40:54 -0500

    Ordering API tested

---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContextSeed.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Infrastructure.Persistence
+{
+    public class OrderContextSeed
+    {
+        public static async Task SeedAsync(OrderContext orderContext, ILogger<OrderContextSeed> logger)
+        {
+            if (!orderContext.Orders.Any())
+            {
+                orderContext.Orders.AddRange(GetPreconfiguredOrders());
+                await orderContext.SaveChangesAsync();
+                logger.LogInformation("Seed database associated with context {DbContextName}", typeof(OrderContext).Name);
+            }
+        }
+
+        private static IEnumerable<Order> GetPreconfiguredOrders()
+        {
+            return new List<Order>
+            {
+                new Order() {UserName = "swn", FirstName = "Mehmet", LastName = "Ozkaya", EmailAddress = "ezozkme@gmail.com", AddressLine = "Bahcelievler", Country = "Turkey", TotalPrice = 350, CardName = "svn", 
+                    CardNumber = "xxxx-xxxx-xxxx-xxxx", CVV = "101", ZipCode = "100001", CreatedBy = "admin", CreatedDate = DateTime.Now,
+                    LastModifiedBy = "admin", LastModifiedDate = DateTime.Now , Expiration = "1", PaymentMethod = 1, State = "4"
+                }
+            };
+        }
+    }
+}
